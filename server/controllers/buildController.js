54aed4d0fbc1ecaf5bbe9cf6ad13a5c0c5a0e395
bomel-official <rusihh228@gmail.com>
2024-02-24 14:21:17 +0300

    fix: sort build attachment list

--- server/controllers/buildController.js
+++ server/controllers/buildController.js
@@ -162,10 +162,7 @@ class BuildController {
                 return res.json({items})
             } else if (object === 'attachment-list') {
                 const {rows: items} = await BuildAttachmentType.findAndCountAll({
-                    order: [[
-                        [{ model: BuildAttachment}, 'title_RU', 'ASC'],
-                        ['title_RU', 'ASC']
-                    ]],
+                    ...getWhere(BuildAttachmentType, req),
                     include: {
                         model: BuildAttachment,
                     }
